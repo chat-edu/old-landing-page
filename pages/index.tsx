0a969b932f4ba58f278dda2b4dceb348ce187da0
json <jason@hedmans.org>
2024-04-26 16:04:30 -0500

    update head

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,8 +8,8 @@ const HomePage: NextPage = () => {
     return (
         <>
             <Head>
-                <title>Rabbithole</title>
-                <meta name="description" content="Generative learning for thinkers, dreamers, and visionaries." />
+                <title>ChatEDU</title>
+                <meta name="description" content="A Second Brain for Students." />
                 <meta name="viewport" content="width=device-width, height=device-height,  initial-scale=1.0, user-scalable=no, user-scalable=0;" />
                 <link rel="icon" href="/logo.ico" />
             </Head>
